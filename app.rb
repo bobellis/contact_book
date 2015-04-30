116cef50de18eb44d1c47a021d43f521beabb12d
Erin Collins & Bob Ellis <student@epicodus.com>
2015-04-30 16:42:32 -0700

    Add method for saving and retreiveing phone numbers, fixing bugs in front end

--- app.rb
+++ app.rb
@@ -30,5 +30,17 @@ end
 
 get('/contact/:id') do
   @contact = Contact.find(params.fetch('id').to_i())
+  @numbers = @contact.get_numbers()
+  erb(:contact)
+end
+
+post('/save_number') do
+  area_code = (params.fetch('area_code')).to_i()
+  number = (params.fetch('number')).to_i()
+  type = params.fetch('type')
+  phone_number = Phone.new(area_code, number, type)
+  @contact = Contact.find((params.fetch("specific_contact_id")).to_i())
+  @contact.add_number(phone_number)
+  @numbers = @contact.get_numbers()
   erb(:contact)
 end
